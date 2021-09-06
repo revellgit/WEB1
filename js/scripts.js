97023a0606773ed30b589638785d3bee2a2f876b
revellgit <mrmattrevell@gmail.com>
2021-09-06 22:59:57 +1200

    Added burger button lines to nav bar, Added js code to add/remove open class on click

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,23 @@ Multiline comment in js
 
 // Single line comment in js
 
-function donothing() {
+function donothing() 
+{
    console.log("hello there from js world!");
+}
+
+function burger()
+{
+   const menu_button = document.querySelector('.nav_burger');
+   let menu_open = false;
+
+   menu_button.addEventListener('click', () => {
+      if(!menu_open) {
+         menu_button.classList.add('open');
+         menu_open = true;
+      } else {
+         menu_button.classList.remove('open');
+         menu_open = false;
+      }
+   })
 }
