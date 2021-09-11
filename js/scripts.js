f1690622f67b117893762b95ff8b08b4e081adc3
revellgit <mrmattrevell@gmail.com>
2021-09-11 23:11:11 +1200

    Started js and css for show/hide of nav menu box, Show/Hide divs for FAQ section

--- js/scripts.js
+++ js/scripts.js
@@ -15,15 +15,35 @@ function donothing()
 function burger()
 {
    const menu_button = document.querySelector('.nav_burger');
+   const nav_menu = document.querySelector('.big_menu');
+
    let menu_open = false;
 
    menu_button.addEventListener('click', () => {
       if(!menu_open) {
-         menu_button.classList.add('open');
+         nav_menu.classList.add('menu_open');
          menu_open = true;
       } else {
-         menu_button.classList.remove('open');
+         nav_menu.classList.remove('menu_open');
          menu_open = false;
+      }
+   })
+}
+
+function opendiv()
+{
+   const div_button = document.querySelector('#button_1');
+   const div_box = document.querySelector('.box_1');
+
+   let div_open = false;
+
+   div_button.addEventListener('click', () => {
+      if(!div_open) {
+         div_box.style.display = 'inline-block';
+         div_open = true;
+      } else {
+         div_box.style.display = 'none';
+         div_open = false;
       }
    })
 }
